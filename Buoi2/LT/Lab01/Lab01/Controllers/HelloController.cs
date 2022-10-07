7ac30a574a887bfc915fc569fc95d4c645b95c0d
= <=>
2022-10-07 16:16:41 +0700

    Buổi 2

---
+++ Buoi2/LT/Lab01/Lab01/Controllers/HelloController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Lab01.Controllers
+{
+    public class HelloController : Controller
+    {
+        // GET: Hello
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //public String Index()
+        //{
+        //    return "Đây là phương thức Index, phương thức mặc định của Controller Hello";
+        //}
+
+        ////public String ChaoMung()
+        ////{
+        ////    return "Đây là phương thức ChaoMung nam trong Controller Hello";
+        ////}
+
+        //public string ChaoMung(string ten, int id = 1)
+        //{
+        //    return ("Xin chao " + ten + ". ID = " + id);
+        //}
+    }
+}
