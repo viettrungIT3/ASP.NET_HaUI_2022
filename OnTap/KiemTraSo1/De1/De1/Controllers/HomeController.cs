24d10e7b7de38ce4cbcef2c9afbd990f1c9b3263
= <=>
2022-11-10 09:28:00 +0700

    Ôn tập kiểm tra số 1 - Đề 1

---
+++ OnTap/KiemTraSo1/De1/De1/Controllers/HomeController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using De1.Models;
+
+namespace De1.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Cau1()
+        {
+            return View();
+        }
+
+        public ActionResult Cau2()
+        {
+            return View();
+        }
+        public ActionResult KetQua(Hang h)
+        {
+            return View(h);
+        }
+    }
+}
---
+++ OnTap/KiemTraSo1/De1/De1/Models/Hang.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace De1.Models
+{
+    public class Hang
+    {
+        public string maHang { get; set; }
+        public string tenHang { get; set; }
+        public string loaiHang { get; set; }
+        public float donGia { get; set; }
+        public int soLuong { get; set; }
+
+        public float ThanhTien()
+        {
+            if (soLuong >= 100)
+            {
+                return (float)(soLuong * donGia * 0.9);
+            }
+            else
+                return soLuong * donGia;
+        }
+    }
+}
