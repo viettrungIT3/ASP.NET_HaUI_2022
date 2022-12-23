05d6eab0e87a7150ad730919015e0bd51b9b90d9
viettrungit3 <viettrungit3@3so365.onmicrosoft.com>
2022-12-24 05:48:39 +0700

    TX2 - init project De3

---
+++ OnTap/KiemTraSo2/NguyenVietTrung_3066_De3/NguyenVietTrung_3066_De3/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace NguyenVietTrung_3066_De3
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ OnTap/KiemTraSo2/NguyenVietTrung_3066_De3/NguyenVietTrung_3066_De3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="NguyenVietTrung_3066_De3.MvcApplication" Language="C#" %>
